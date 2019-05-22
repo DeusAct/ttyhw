a43e01686dd5a20a769e2391f9fc80b3234d6f8f
viktoria.tisler <viktoria.tisler@ttu.ee>
2019-05-22 20:55:24 +0300

    hehehe beri

--- IdeaProjects/TextProcessing/src/Main.java
+++ IdeaProjects/TextProcessing/src/Main.java
@@ -3,20 +3,14 @@ import java.util.*;
 
 public class Main {
 
-    //public static void main(String[] args) throws IOException {
-
-    //        List<String> list = Files.readAllLines(Paths.get("The_Last_of_the_Mohicans-James_Fenimore_Cooper.txt"));
-//
-//
-//        Set<String> unique = new HashSet<>(list);
-//        for (String key : unique) {
-//            System.out.println(key + ": " + Collections.frequency(list, key));
-//        }
-
     public static void main(String[] args) throws IOException {
 
-        Map <String, Integer> wordCounter = new HashMap<>();
-        Scanner s = new Scanner(new File("The_Last_of_the_Mohicans-James_Fenimore_Cooper.txt"));
+        /**
+         * Ex. 1:
+         */
+
+        Map<String, Integer> wordCounter = new HashMap<>();
+        Scanner s = new Scanner(new File("IdeaProjects/TextProcessing/The_Last_of_the_Mohicans-James_Fenimore_Cooper.txt"));
         while (s.hasNext()) {
             String next = s.next();
             Integer count = wordCounter.get(next);
@@ -28,30 +22,14 @@ public class Main {
         }
         s.close();
 
-//        Map <String, Integer> wordCounter = new HashMap<>();
-//        Scanner s = new Scanner(new File("The_Last_of_the_Mohicans-James_Fenimore_Cooper.txt"));
-//        while (s.hasNext()) {
-//            String next = s.next();
-//            Integer count = wordCounter.get(next);
-//            if (count != null) {
-//                wordCounter.put(next, 1);
-//            } else {
-//                wordCounter.put(next, 0);
-//            }
-//        }
-//        s.close();
 
-        for (Map.Entry<String, Integer> entry : wordCounter.entrySet()) {
-            String key = entry.getKey();
-            Integer value = entry.getValue();
-
-            System.out.println ("Key: " + key + "," + " Value: " + value);
-        }
-
-        System.out.println(" ");
-        for (int i=0; i<10; i++){
+        System.out.println(wordCounter.entrySet());
+        for (int i = 0; i < 10; i++) {
             System.out.println("-----------------------------------------");
         }
+        /**
+         * Ex. 3:
+         */
 
         System.out.println("Unique values: ");
         System.out.println(" ");
@@ -60,10 +38,52 @@ public class Main {
             String key = entry.getKey();
             Integer value = entry.getValue();
 
-            if(value == 1) {
+            if (value == 1) {
                 System.out.println(key);
             }
         }
+        wordCounter.clear();
+
+        /**
+         * Ex. 2:
+         */
+
+        List<Character> alphabet = new ArrayList<>();
+        List<String> words = new ArrayList<>();
+        for(Character c = 'a'; c <= 'z'; c++) {
+            alphabet.add(c);
+        }
+        System.out.println(alphabet);
+        s = new Scanner(new File("IdeaProjects/TextProcessing/The_Last_of_the_Mohicans-James_Fenimore_Cooper.txt"));
+
+        while (s.hasNext()) {
+            String next = s.next();
+            // []+  - 1 or more times
+            // []* - 0 or more times
+
+            words.add(next);
+
+            if (next.startsWith("'")  || next.endsWith("'") || next.endsWith(",") || next.endsWith(".")) {
+                next = next.replace("'", "");
+                next = next.replace(".", "");
+                next = next.replace(",", "");
+            }
+            for (Character letter : alphabet) {
+                if (next.toLowerCase().startsWith(String.valueOf(letter))) {
+                    if (wordCounter.containsKey(String.valueOf(letter))) {
+                        wordCounter.put(String.valueOf(letter), wordCounter.get(String.valueOf(letter)) + 1);
+                    } else {
+                        wordCounter.put(String.valueOf(letter), 1);
+                    }
+                }
+            }
+
+        }
+        s.close();
+        System.out.println(wordCounter.entrySet());
+        System.out.println(words);
+
+
     }
 }
 
